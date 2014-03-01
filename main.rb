09b2d4791c3f82b9d120800771c03f8e98aff708
shgtkshruch <shgtk.shruch@gmail.com>
2014-03-01 18:17:48 +0900

    use before/after

--- main.rb
+++ main.rb
@@ -2,15 +2,27 @@ require 'sinatra'
 require 'sinatra/reloader'
 require 'slim'
 
+before '/admin' do
+  @msg = 'admin area!'
+end
+
+before do
+  @author = "shgtkshruch"
+end
+
+after do
+  logger.info "page displayed successfully"
+end
+
 get '/' do
   @title = "main index"
-  @content = "main content"
+  @content = "main content by " + @author
   slim :index  
 end
 
 get '/about' do
   @title = "about this page"
-  @content = "this page is ..."
+  @content = "this page is ... by " + @author
   @email = "example@gmail.com"
   slim :about
 end
